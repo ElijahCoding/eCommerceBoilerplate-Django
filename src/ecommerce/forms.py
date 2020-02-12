6fa459f53a8e3e1d080174cf704cb13694c817bf
ElijahCoding <cuiyeqing960904@gmail.com>
2020-02-13 00:08:07 +0300

    validation in forms

--- src/ecommerce/forms.py
+++ src/ecommerce/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class ContactForm(forms.Form):
     fullname = forms.CharField(
@@ -43,4 +46,23 @@ class RegisterForm(forms.Form):
     password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
 
     def clean_username(self):
-        pass
+        username = self.cleaned_data.get('username')
+        qs = User.objects.filter(username=username)
+        if qs.exists():
+            raise forms.ValidationError("Username is taken")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        qs = User.objects.filter(email=email)
+        if qs.exists():
+            raise forms.ValidationError("email is taken")
+        return email
+
+    def clean(self):
+        data = self.cleaned_data
+        password = self.cleaned_data.get('password')
+        password2 = self.cleaned_data.get('password2')
+        if password2 != password:
+            raise forms.ValidationError("Passwords must match.")
+        return data
