dc89b97a06e30d34ff817ddc87efff9cc9155966
ElijahCoding <cuiyeqing960904@gmail.com>
2020-02-13 21:17:27 +0300

    custom model manager

--- src/products/views.py
+++ src/products/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.http import Http404
 from django.views.generic import ListView, DetailView
 from .models import Product
 
 # Create your views here.
 class ProductListView(ListView):
-    queryset = Product.objects.all()
+    # queryset = Product.objects.all()
     template_name = "products/list.html"
 
     def get_context_data(self, *args, **kwargs):
@@ -12,11 +13,27 @@ class ProductListView(ListView):
         print(context)
         return context
 
+    def get_queryset(self):
+        return Product.objects.all()
+
 class ProductDetailView(DetailView):
-    queryset = Product.objects.all()
+    # queryset = Product.objects.all()
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
         return context
+
+    # def get_object(self, *args, **kwargs):
+    #     request = self.request
+    #     pk = self.kwargs.get('pk')
+    #     instance = Product.objects.get_by_id(pk)
+    #     if instance is None:
+    #         raise Http404('Product doesnt exist')
+    #     return instance
+
+    def get_queryset(self):
+        request = self.request
+        pk = self.kwargs.get('pk')
+        return Product.objects.filter(pk=pk)
