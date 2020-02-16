596e9dc0c78ec3d82769de4870da1bdff5fcfda6
ElijahCoding <cuiyeqing960904@gmail.com>
2020-02-16 19:10:51 +0300

    cart model built

--- src/carts/models.py
+++ src/carts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+from products.models import Product
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True)
+    products = models.ManyToManyField(Product, blank=True)
+    subtotal = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+    total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
