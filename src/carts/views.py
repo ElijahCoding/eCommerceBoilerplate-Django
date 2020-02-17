e8c1cf0f66551ac9e3d562d30a4d3fdfd14cf8e4
ElijahCoding <cuiyeqing960904@gmail.com>
2020-02-17 16:02:53 +0300

    building cart views

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Cart
 
 def cart_home(request):
+    cart_id = request.session.get('cart_id', None)
+
+    if cart_id is None:
+        cart_obj = Cart.objects.create(user=None)
+        request.session['cart_id'] = cart_obj.id
+    else:
+        qs = Cart.objects.filter(id=cart_id)
+        cart_obj = Cart.objects.get(id=cart_id)
+
     return render(request, 'carts/home.html', {})
 
 def cart_update(request):
