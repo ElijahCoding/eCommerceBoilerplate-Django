e961b4a89bc0179bcaf35278d5b514cfcdbe7152
ElijahCoding <cuiyeqing960904@gmail.com>
2020-02-17 22:26:35 +0300

    cart in session built

--- src/carts/views.py
+++ src/carts/views.py
@@ -2,19 +2,9 @@ from django.shortcuts import render
 from .models import Cart
 
 def cart_home(request):
-    cart_id = request.session.get('cart_id', None)
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
 
-    qs = Cart.objects.filter(id=cart_id)
-    if qs.count() == 1:
-        cart_obj = qs.first()
-        if request.user.is_authenticated() and cart_obj.user is None:
-            cart_obj.user = request.user
-            cart_obj.save()
-    else:
-        cart_obj = Cart.objects.new(user=request.user)
-        request.session['cart_id'] = cart_obj.id
-
-    return render(request, 'carts/home.html', {})
+    return render(request, 'carts/home.html', { "cart": cart_obj })
 
 def cart_update(request):
     pass
