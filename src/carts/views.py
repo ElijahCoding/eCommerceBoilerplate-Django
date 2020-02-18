b455cf5f42ef12c9abf184663c7fafda75a652b2
ElijahCoding <cuiyeqing960904@gmail.com>
2020-02-18 10:08:31 +0300

    cart update view

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Cart
+from products.models import Product
 
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
