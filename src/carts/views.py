bebdacc45ed7e61a3ae61a747ff0d346dc8230ac
ElijahCoding <cuiyeqing960904@gmail.com>
2020-02-18 22:35:29 +0300

    add and remove product in cart"

--- src/carts/views.py
+++ src/carts/views.py
@@ -8,4 +8,13 @@ def cart_home(request):
     return render(request, 'carts/home.html', { "cart": cart_obj })
 
 def cart_update(request):
-    pass
+    product_id = request.POST.get('product_id')
+    if product_id is not None:
+        product_obj = Product.objects.get(id=product_id)
+        cart_obj, new_obj = Cart.objects.new_or_get(request)
+        if product_obj in cart_obj.products.all():
+            cart_obj.products.remove(product_obj)
+        else:
+            cart_obj.products.add(product_obj)
+    
+    return redirect("cart:home")
