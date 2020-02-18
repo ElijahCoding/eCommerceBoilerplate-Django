3d24bacbba476b95f3dcc708b9787212b0b1a35c
ElijahCoding <cuiyeqing960904@gmail.com>
2020-02-18 22:50:48 +0300

    showing cart

--- src/carts/views.py
+++ src/carts/views.py
@@ -10,11 +10,17 @@ def cart_home(request):
 def cart_update(request):
     product_id = request.POST.get('product_id')
     if product_id is not None:
-        product_obj = Product.objects.get(id=product_id)
+        try:
+            product_obj = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            print("Show message to user, product is gone?")
+            return redirect("cart:home")
         cart_obj, new_obj = Cart.objects.new_or_get(request)
         if product_obj in cart_obj.products.all():
             cart_obj.products.remove(product_obj)
         else:
             cart_obj.products.add(product_obj)
+        
+        request.session['cart_items'] = cart_obj.products.count()
     
     return redirect("cart:home")
