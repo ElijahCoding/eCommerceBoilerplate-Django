c05a1cdf39c0ea385556bddc7f44a132966d8b85
ElijahCoding <cuiyeqing960904@gmail.com>
2020-02-19 21:32:50 +0300

    building cart signals

--- src/orders/models.py
+++ src/orders/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from carts.models import Cart
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from ecommerce.utils import unique_order_id_generator
 
 ORDER_STATUS_CHOICES = (
@@ -21,8 +21,25 @@ class Order(models.Model):
     def __str__(self):
         return self.order_id
 
+    def update_total(self):
+        return
+
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
     if not instance.order_id:
         instance.order_id = unique_order_id_generator(instance)
 
 pre_save.connect(pre_save_create_order_id, sender=Order)
+
+def post_save_cart_total(sender, instance, *args, **kwargs):
+    cart_obj = instance
+    cart_total = cart_obj.total
+    cart_id = cart_obj.id
+    qs = Order.objects.filter(cart__id=cart_id)
+    if qs.count() == 1:
+        order_obj = qs.first()
+        order_obj.update_total()
+        
+post_save.connect(post_save_cart_total, sender=Cart)
+
+def post_save_order(sender, instance, created, *args, **kwargs):
+    pass
