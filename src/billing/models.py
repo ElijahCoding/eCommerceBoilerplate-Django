d5b1e437b6af01536d8979a52cdf0fff0487e9b5
ElijahCoding <cuiyeqing960904@gmail.com>
2020-02-20 21:23:16 +0300

    doing billing profile in checkout view

--- src/billing/models.py
+++ src/billing/models.py
@@ -1,4 +1,27 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import pre_save, post_save
+
+User = settings.AUTH_USER_MODEL
+
+class BillingProfileManager(models.Manager):
+    def new_or_get(self, request):
+        ass
 
 class BillingProfile(models.Model):
-    pass
+    user = models.OneToOneField(User, null=True, blank=True)
+    email = models.EmailField()
+    active = models.BooleanField(default=True)
+    update = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.email
+    
+    objects = BillingProfileManager()
+    
+def user_created_receiver(sender, instance, created, *args, **kwargs):
+    if created and instance.email:
+        BillingProfile.objects.get_or_create(user=instance)
+
+post_save.connect(user_created_receiver, sender=User)
