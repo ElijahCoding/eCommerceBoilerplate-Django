409c98edd2e2d53f0fc4a154f39fbcb5082b5af8
ElijahCoding <cuiyeqing960904@gmail.com>
2020-02-20 22:47:13 +0300

    building guest login view

--- src/carts/views.py
+++ src/carts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Cart
 from orders.models import Order
 from products.models import Product
-from accounts.forms import LoginForm
+from accounts.forms import LoginForm, GuestForm
 from billing.models import BillingProfile
 
 def cart_home(request):
@@ -40,6 +40,7 @@ def checkout_home(request):
     billing_profile = None
 
     login_form = LoginForm()
+    guest_form = GuestForm()
 
     if user.is_authenticated():
         billing_profile, billing_profile_created = BillingProfile.objects.get_or_create(user=user, email=user.email)
