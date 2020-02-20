417957f9ff9fc8de03564fcd98c92f340f39288a
ElijahCoding <cuiyeqing960904@gmail.com>
2020-02-20 20:12:23 +0300

    checking redirected url after login

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import LoginForm, RegisterForm
+from django.contrib.auth import authenticate, login, get_user_model
+from django.utils.http import is_safe_url
 
-# Create your views here.
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form": form
+    }
+
+    next_ = request.GET.get('next')
+    next_post = request.POST.get('next')
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            print('Error')
+    return render(request, "accounts/login.html", context)
+
+User = get_user_model()
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = { "form": form }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
+    return render(request, "accounts/register.html", context)
--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from .views import home_page, about_page, contact_page, login_page, register_page
+from .views import home_page, about_page, contact_page
+from accounts.views import login_page, register_page
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
